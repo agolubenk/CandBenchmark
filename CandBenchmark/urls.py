370e42cb3a6ddee8bc93a4251883975ad6c2900e
Andrei Golubenko <95508920+agolubenk@users.noreply.github.com>
2025-03-22 00:16:15 +0300

    Незначительные изменения настроек и визуалов

--- CandBenchmark/urls.py
+++ CandBenchmark/urls.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
-
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('admin/', admin.site.urls),
     path('', include('vacancies.urls')),
     path('hh/', include('hhru.urls')),
-]
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('profile/', TemplateView.as_view(template_name='registration/profile.html'), name='profile'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
