df7659f5ced4f3718f533a37422bf5c853fca82b
Andrei Golubenko <95508920+agolubenk@users.noreply.github.com>
2025-03-21 23:46:13 +0300

    Незначительные изменения настроек и визуалов

--- hhru/admin.py
+++ hhru/admin.py
@@ -3,7 +3,7 @@ from .models import Vacancyhh
 
 @admin.register(Vacancyhh)
 class VacancyhhAdmin(admin.ModelAdmin):
-    list_display = ('hh_id', 'title', 'description', 'salary_from', 'salary_to', 'currency', 'area', 'created_at')
+    list_display = ('hh_id', 'title', 'salary_from', 'salary_to', 'currency', 'area', 'created_at')
 
     search_fields = ('hh_id', 'title', 'description', 'salary_from', 'salary_to', 'currency', 'area', 'created_at')
-    list_filter = ('title', 'salary_from', 'salary_to', 'currency', 'area', 'created_at')
+    list_filter = ('currency', 'area', 'created_at')
